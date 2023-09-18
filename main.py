9557e6b45c95402e8e224ff8702acf837ee28351
Krrish Dholakia <krrishdholakia@gmail.com>
2023-09-18 16:36:49 -0700

    fix cors error

--- main.py
+++ main.py
@@ -19,15 +19,10 @@ from fastapi.responses import StreamingResponse
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
-origins = [
-    "http://localhost:3000",  # React app
-    "http://localhost:8080",  # Local
-    "https://docs.litellm.ai/",  # Production React app
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
