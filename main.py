1777930d96238466d8d6acb12233f025e45b6dc6
Krrish Dholakia <krrishdholakia@gmail.com>
2023-09-19 13:13:27 -0700

    clean up code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # sys.path.insert(
 #     0, os.path.abspath("../")
 # )  # Adds the parent directory to the system path
-
+import os 
 import secrets
 import traceback
 import llm as llm
@@ -13,7 +13,7 @@ import litellm
 from litellm import BudgetManager
 litellm.max_budget = 1000 
 
-budget_manager = BudgetManager(project_name="litellm_proxy", client_type="hosted")
+budget_manager = BudgetManager(project_name=os.getenv("PROJECT_NAME"), client_type="hosted")
 
 from fastapi import FastAPI, Request, status, HTTPException, Depends
 from fastapi.responses import StreamingResponse
@@ -29,18 +29,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# supabase
-from supabase import create_client, Client
-import os
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-
-supabase: Client = create_client(url, key)
-
 user_api_keys = set(budget_manager.get_users())
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# Utils for Auth 
+######## AUTH UTILITIES ################
+
 def user_api_key_auth(api_key: str = Depends(oauth2_scheme)):
     if api_key not in user_api_keys:
         raise HTTPException(
@@ -50,7 +43,7 @@ def user_api_key_auth(api_key: str = Depends(oauth2_scheme)):
         )
 
 
-def fastrepl_auth(api_key: str = Depends(oauth2_scheme)):
+def key_auth(api_key: str = Depends(oauth2_scheme)):
     print(api_key)
     print(os.getenv("USERS_KEYS"))
     user_keys = os.getenv("USERS_KEYS").split(",")
@@ -60,7 +53,8 @@ def fastrepl_auth(api_key: str = Depends(oauth2_scheme)):
             detail={"error": "invalid admin key"},
             # TODO: this will be {'detail': {'error': 'something'}}
         )
-# end of utils for auth
+
+######## CHAT COMPLETIONS ################
 
 # for streaming
 def data_generator(response):
@@ -90,63 +84,23 @@ async def completion(request: Request):
     return response
 
 
-# Endpoint for adding new LLMs
-# use this to store your openai key
-# expects data to be {'provider': '', 'key': ''}
-@app.post("/litellm/add_key", dependencies=[Depends(fastrepl_auth)])
-async def save(request: Request):
-    try:
-        print("got add key")
-        litellm_user_key = request.headers.get("Authorization").replace("Bearer ", "")  # type: ignore
-        data = await request.json()
-        print(data)
-        provider = data.get("provider", "")
-        key = data.get("key", "")
-    except:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
-
-    provider = provider.upper() + "_API_KEY"
-    try:
-        data = supabase.table('llm_api_keys').select("llm_keys").eq("admin_key", litellm_user_key).execute().data
-        if len(data)>0:
-            # need to update existing json for user:
-            llm_keys = data[0].get('llm_keys', {})
-            llm_keys[provider] = key
-            supabase_data_obj = {
-                "llm_keys": llm_keys
-            }
-            supabase.table("llm_api_keys").update(supabase_data_obj).eq("admin_key", litellm_user_key).execute()
-        else:
-            supabase_data_obj = {
-                "admin_key": litellm_user_key,
-                "llm_keys": { provider: key}
-            }
-            supabase.table("llm_api_keys").insert(supabase_data_obj).execute()
-    except Exception as e:
-        traceback.print_exc()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    return {"status": "saved key", "key_name": provider}
-
+@app.get("/models/available")
+def get_available_models():
+    return {"models": litellm.utils.get_valid_models()}
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
 
+######## KEY MANAGEMENT ################
 
-@app.get("/cost/reset", dependencies=[Depends(user_api_key_auth)])
-async def report_reset(request: Request):
-    key = request.headers.get("Authorization").replace("Bearer ", "")  # type: ignore
-    return budget_manager.reset_cost(key)
-
-
-@app.get("/cost/current", dependencies=[Depends(user_api_key_auth)])
+@app.get("/key/cost", dependencies=[Depends(user_api_key_auth)])
 async def report_current(request: Request):
     key = request.headers.get("Authorization").replace("Bearer ", "")  # type: ignore
     return budget_manager.get_model_cost(key)
 
 
-@app.post("/key/new", dependencies=[Depends(fastrepl_auth)])
+@app.post("/key/new", dependencies=[Depends(key_auth)])
 async def generate_key(request: Request):
     try:
         data = await request.json()
